72a59b0488185a6a293d773e107a19c8a1f705da
anaroshi <anaroshi@naver.com>
2023-04-07 23:22:45 +0900

    webpack-template(기본구조)

---
+++ 0.projectSample/ResponseCheck.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div id="screen">{{}}</div>
+        <div>
+            <div>평균 시간 : {{}}</div>
+            <button @click.prevent="onReset">리셋</button>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        methods: {
+            onReset() {     
+            }
+        }
+    };
+</script>
+
+<style>
+</style>
