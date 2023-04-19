4e603cbc0e8b1bd6307128bc4786d577e4c83df8
anaroshi <anaroshi@naver.com>
2023-04-19 16:56:59 +0900

    Reusable Components with Slots

---
+++ lesson/ReusableComponentsWithSlots/ReusableComponent.vue
@@ -0,0 +1,78 @@
+<template>
+<!--    <AppLayout>
+        <template #default> // # === v-slot:
+            <AppButton @click="log">
+                <Icon />
+                add to favorites
+            </AppButton>
+        </template>
+    </AppLayout> 
+--> 
+<div class="page">
+    <select v-model="selected">
+        <option v-for="option in options" :key="option.value" :value="option.value">{{option.label}}</option>
+    </select>
+    <AppUserList>        
+        <template #userlist="{list: users, remove}">
+            <AppUserCardsList :list="users">
+                <template #[selected]="{text}">
+                    <h4>{{ text }}</h4>
+                </template>
+                <template #secondrow="{item: user}">
+                    <button @click="remove(user)">{{ user.name.first }}</button>
+                </template>
+            </AppUserCardsList>
+        </template>
+    </AppUserList>
+</div>
+</template>
+
+<script>
+    import AppUserCardsList from './components/AppUserCardsList';
+    import AppUserList from './components/AppUserList'
+    import AppButton from './components/AppButton'
+
+    export default {
+        components: {
+            AppUserCardsList,
+            AppUserList,
+            AppButton
+        },
+        data() {
+            return {
+                selected: "first",
+                options: [
+                    {value: "first", label:"first name"},
+                    {value: "last", label:"last name"},
+                    {value: "full", label:"full name"},
+                    {value: "fullWithTitle", label:"full name with title"}
+                ]
+            }
+        } 
+    };
+    // import AppLayout from './components/AppLayout'
+    // import AppButton from './components/AppButton'
+    // import Icon from './components/Icon'
+    // export default {
+    //     components: {
+    //         AppLayout,
+    //         AppButton,
+    //         Icon
+    //     },
+    //     methods: {
+    //         log() {
+    //             console.log("works")
+    //         }
+    //     }
+    // }
+    
+</script>
+
+<style>
+.page {
+  padding: 2rem;
+}
+.page > * + * {
+  margin-top: 2rem;
+}
+</style>
---
+++ lesson/ReusableComponentsWithSlots/components/ComplexButton.vue
@@ -0,0 +1,49 @@
+<template>
+    <button 
+        class="button"
+        :disable="disabled"
+        :class="{
+            primary: primary,
+            secondary: secondary,
+            error: error
+        }"
+    >
+        <template v-if="!error">
+            <template v-if="!loading">
+                <Icon :color="!disabled ? defaultIconColor : loadingIconColor" />
+                {{ loadingText }}
+            </template>
+            <template v-else>
+                <Icon v-if="showLeftIcon" :color="!disabled ? defaultIconColor : leftIconColor" :name="leftIconName" />
+                {{ text }}
+                <Icon v-if="showRightIcon" :color="!disabled ? defaultIconColor : rightIconColor" :name="rightIconName" />
+            </template>
+        </template>
+        <template v-else>
+            {{ errorText }}
+        </template>
+    </button>
+</template>
+
+<script>
+export default {
+    props: [
+        "text",
+        "loadingText",
+        "errorText",
+        "primary",
+        "secondary",
+        "disabled",
+        "loading",
+        "error",
+        "defaultIconColor",
+        "loadingIconColor",
+        "leftIconColor",
+        "leftIconName",
+        "rightIconColor",
+        "rightIconName",
+        "showLeftIcon",
+        "showRightIcon"
+    ]
+};
+</script>
